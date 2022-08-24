1403af32ae14ccb295e9bd038c26164e51394d57
roberttoth9000 <roberttoth9000@gmail.com>
2022-08-24 23:00:27 +0200

    fix structure, add landing-page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/pages/registration/registration.module').then(
+        (mod) => mod.RegistrationModule
+      ),
+  },
+  {
+    path: 'registration',
+    loadChildren: () =>
+      import('./modules/pages/registration/registration.module').then(
+        (mod) => mod.RegistrationModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

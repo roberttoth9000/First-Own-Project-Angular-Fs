af5d7e86cff5b45d51eb76b50e70f97fc550a3db
roberttoth9000 <roberttoth9000@gmail.com>
2022-08-24 23:00:46 +0200

    fix structure, add landing page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.modules';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { RegistrationComponent } from './app/modules/pages/registration/registration.component';
+import { RegistrationComponent } from './modules/pages/registration/registration.component';
+import { LandingPageComponent } from './modules/pages/landing-page/landing-page.component';
+// import { HeaderComponent } from './shared/components/header/header.component';
+// import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    RegistrationComponent
-  ],
+  declarations: [AppComponent, RegistrationComponent, LandingPageComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

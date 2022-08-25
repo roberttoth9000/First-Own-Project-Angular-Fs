20ab650cefa20bf21b7d906864c79ca49bf3aeff
roberttoth9000 <roberttoth9000@gmail.com>
2022-08-25 22:43:31 +0200

    usercontroller + services communication

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import IRegisterUserRequest from "../models/IRegisterUserRequest";
+import { emailService } from "../services/emailService";
+import { passwordService } from "../services/passwordService";
 
 export const userController = {
   async registerUser(
@@ -12,5 +14,34 @@ export const userController = {
       email: req.body.email,
       password: req.body.password,
     };
+    if (req.body.length === 0) {
+      return next({
+        message: "Username, email and password are required.",
+        status: 422,
+      });
+    }
+
+    const isValidEmail: boolean = emailService.emailCheck(
+      newRegistration.email
+    );
+
+    if (!isValidEmail) {
+      return next({
+        message: "Email is invalid.",
+        status: 422,
+      });
+    }
+
+    const isValidPassword: boolean = passwordService.checkPassword(
+      newRegistration.password
+    );
+
+    if (!isValidPassword) {
+      return next({
+        message:
+          "Password must be at least 8 characters and contain letter and number.",
+        status: 422,
+      });
+    }
   },
 };

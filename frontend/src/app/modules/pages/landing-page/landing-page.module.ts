9661ae8c7eccf8916144810bf2dc6e39d491fccd
roberttoth9000 <roberttoth9000@gmail.com>
2022-08-26 12:59:23 +0200

    landing page feature 2

--- frontend/src/app/modules/pages/landing-page/landing-page.module.ts
+++ frontend/src/app/modules/pages/landing-page/landing-page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
+import { SharedModule } from 'src/app/shared/shared.modules';
 
 @NgModule({
   declarations: [LandingPageComponent],
-  imports: [CommonModule, LandingPageRoutingModule],
+  imports: [CommonModule, LandingPageRoutingModule, SharedModule],
 })
 export class LandingPageModule {}

d270d4a6fcfef7874dd1c85d324c646902a2bc89
roberttoth9000 <roberttoth9000@gmail.com>
2022-08-26 17:46:08 +0200

    mocked boardgame listed on landing page

---
+++ frontend/src/app/core/models/IBoardGameViewModel.ts
@@ -0,0 +1,16 @@
+export interface IBoardGameViewModel {
+  id: number;
+  title: string;
+  price: number;
+  type: string;
+  exchange: string;
+  length: string;
+  difficulty: string;
+  condition: string;
+  description: string;
+  imgUrl: string;
+  user_id: number;
+  player: string;
+  age: string;
+  email: string;
+}
--- frontend/src/app/shared/components/board-game/board-game.component.ts
+++ frontend/src/app/shared/components/board-game/board-game.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IBoardGameViewModel } from 'src/app/core/models/IBoardGameViewModel';
 
 @Component({
   selector: 'app-board-game',
   templateUrl: './board-game.component.html',
-  styleUrls: ['./board-game.component.scss']
+  styleUrls: ['./board-game.component.scss'],
 })
 export class BoardGameComponent implements OnInit {
+  @Input() boardGameData: IBoardGameViewModel = {
+    id: 3,
+    title: '7 csoda',
+    price: 12990,
+    type: 'kaland',
+    exchange: 'igen',
+    length: '2-4 óra',
+    difficulty: 'közepes',
+    condition: 'bontatlan',
+    description:
+      'Lorem ipsum galileo bikini ahol a madár se jár, vagy még az sem, de lehet mégis, mert nehogymár. A magyar könnyűzene abszolút sikerei, amelyek megkérdőjelezhetetlenek, légyszi kontaktálj velem, ha mindezek után érdekel a társas',
+    imgUrl:
+      'https://tarsasjatekok.com/files/common/d/d3/d39/d393f54c3de29e7984cee072b3538872/tarsas-7csodaalap-lrg.jpg',
+    user_id: 1,
+    player: '2-4',
+    age: '10-99',
+    email: 'valami@gmail.com',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

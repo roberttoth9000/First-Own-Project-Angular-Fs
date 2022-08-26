d6e8625d7b56839ed0378a41b429c4b082881122
roberttoth9000 <roberttoth9000@gmail.com>
2022-08-26 19:21:19 +0200

    BE routes to the get /board-games

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -3,6 +3,7 @@ import requestBodyValidator from "../middlewares/requestBodyValidator";
 import { registerUserRequestKeys } from "../helpers/requestKeys";
 import { userController } from "../controllers/userController";
 import cors from "cors";
+import { boardGameController } from "../controllers/boardGameController";
 
 const apiRouter = express.Router();
 apiRouter.use(express.json());
@@ -13,5 +14,8 @@ apiRouter
   .route("/register")
   .post(requestBodyValidator(registerUserRequestKeys, 400));
 apiRouter.post("/register", userController.registerUser);
+
+//boardGames
+apiRouter.get("/board-games", boardGameController.getAllBoardgames);
 
 export default apiRouter;
